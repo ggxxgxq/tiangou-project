21c51781577ac223b57182b1ce6a41afc4cbcdcc
ggxxgxq <1356857447@qq.com>
2018-03-13 18:18:41 +0800

    初步完成首页制作

--- tiangou-project/src/components/Home.vue
+++ tiangou-project/src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <article>
+  <article class="bodyWrap">
   	<div id="topWrap">
 		<div class="top">
 			<i class="iconfont icon-iconset0256 text-white"></i>
@@ -54,7 +54,7 @@
 				</a>
 			</div>
 		</div>
-		<div class="spe_title">
+		<div class="spe_title title">
 			<div >
 				<a href="">
 					<img :src="'https:'+listSpe" />
@@ -94,17 +94,141 @@
 		<div class="groop_wrap">
 			<div class="groop_out">
 				<div class="groop_book">
-					<div class="brand_sale">
-						<div class="brand">
-							<a href="">
-								<img :src="listGroop.imageUrl">
-							</a>
+					<div class="groop_inner">
+						<div class="brand_sale" v-for="item in listGroop">
+							<div class="brand">
+							   <main>
+								<a href="">
+									<img :src="item.imageUrl">
+								</a>
+								</main>
+							</div>
+						</div>						
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="qua title">
+			<div >
+				<a href="">
+					<img :src="'https:'+listQua" />
+				</a>
+			</div>
+		</div>
+		<div class="qShopWrap">
+			<div class="qShop_out">
+				<div class="qshop">
+					<div class="qshop_left">
+						<a href=""><img :src="listQshop[0].imageUrl"></a>
+					</div>
+					<div class="qshop_right">
+						<a href=""><img :src="listQshop[1].imageUrl"></a>
+						<a href="">
+				      		<span>
+								<a href=""><img :src="listQshop[2].imageUrl"></a>
+								<a href=""><img :src="listQshop[3].imageUrl"></a>
+				      			
+				      		</span>
+				      	</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="act title">
+			<div >
+				<a href="">
+					<img :src="'https:'+listAct" />
+				</a>
+			</div>
+		</div>
+		<div class="act_wrap">
+			<div class="actBaby_wrap wrap_common">
+				<div class="act_baby p_common" > 
+					<div class="babyClass img_common">
+						<a href=""><img :src="'https:'+listbaby"></a>
+					</div>
+					<div class="act_content con_common">
+						<div class="act_shop shop_common" v-for="item in listContent1">
+							<a href=""><img :src="item.imageUrl"></a>
 						</div>
-						<div class="groop">
-							<a href="">
-								<img :src="listGroop.imageUrl">
-							</a>
+					</div>
+				</div>
+			</div>
+			<div class="dcountWrap wrap_common">
+				<div class="discount p_common">
+					<div class="discount img_common">
+						<a href=""><img :src="'https:'+listdis"></a>
+					</div>
+					<div class="dis_content con_common">
+						<div class="dis_shop shop_common" v-for="item in listContent2">
+							<a href=""><img :src="item.imageUrl"></a>
 						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="actBaby_wrap wrap_common">
+				<div class="act_baby p_common" > 
+					<div class="babyClass img_common">
+						<a href=""><img :src="'https:'+listeat"></a>
+					</div>
+					<div class="act_content con_common">
+						<div class="act_shop shop_common" v-for="item in listContent3">
+							<a href=""><img :src="item.imageUrl"></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="dcountWrap wrap_common">
+				<div class="discount p_common">
+					<div class="discount img_common">
+						<a href=""><img :src="'https:'+listworker"></a>
+					</div>
+					<div class="dis_content con_common">
+						<div class="dis_shop shop_common" v-for="item in listContent4">
+							<a href=""><img :src="item.imageUrl"></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+        <div class="info title">
+			<div >
+				<a href="">
+					<img :src="'https:'+listInfo" />
+				</a>
+			</div>
+		</div>
+		<div class="InfoWrap">
+			<div class="Infoout">
+				<div class="Infolist">
+					<div class="slist" v-for="item in listContent5" v-lazy="item">
+						<section>
+							<a href="">
+								<div class="shop_msg">
+									<img :src="item.data.imageUrl">
+								</div>
+								<div class="shop_detail">
+									<span>海外购</span>
+									<span>{{item.data.title}}</span>
+								</div>
+								<span class="s-price">￥{{item.data.price}}</span>
+							</a>
+						</section>
+					</div>
+					<div class="slist" v-for="item in listContent5List" v-lazy="item">
+						<section>
+							<a href="">
+								<div class="shop_msg">
+									<img :src="item.data.imageUrl">
+								</div>
+								<div class="shop_detail">
+									<span>海外购</span>
+									<span>{{item.data.title}}</span>
+								</div>
+								<span class="s-price">￥{{item.data.price}}</span>
+							</a>
+						</section>
 					</div>
 				</div>
 			</div>
@@ -126,8 +250,22 @@ export default {
       listOne:"",
       listSpe:"",
       listLimit:[],
-      listGroop:""
-
+      listGroop:[],
+      listQua:[],
+      listQshop:[],
+      listAct:[],
+      listbaby:[],
+      listContent1:[],
+      listdis:[],
+      listContent2:[],
+      listeat:[],
+      listContent3:[],
+      listworker:[],
+      listContent4:[],
+      listInfo:[],
+      listContent5:[],
+      listContent5List:[]
+     
     }
   },
   mounted(){
@@ -138,8 +276,30 @@ export default {
   		this. listOne = res.data.data[8].data.items[0].imageUrl;
   		this. listSpe = res.data.data[10].data.items[0].imageUrl;
   		this. listLimit = res.data.data[11].data.items;
-  		this. listGroop = res.data.data[13].data.items[0];
-  		console.log(res.data.data[13].data.items[0])
+  		this. listGroop = res.data.data[13].data.items;
+  		this. listQua = res.data.data[15].data.items[0].imageUrl;
+  		this. listQshop = res.data.data[16].data.items;
+  		this.listAct = res.data.data[18].data.items[0].imageUrl;
+  		this.listbaby = res.data.data[19].data.items[0].imageUrl;
+  		this.listContent1 = (res.data.data[19].data.items).slice(1);
+  	    this.listdis = res.data.data[20].data.items[0].imageUrl;
+  		this.listContent2 = (res.data.data[20].data.items).slice(1);
+  		this.listeat = res.data.data[21].data.imageUrl;
+  		this.listContent3 = res.data.data[21].data.items;
+  	    this.listworker = res.data.data[23].data.imageUrl;
+  		this.listContent4 = res.data.data[23].data.items;
+  		this.listInfo = res.data.data[25].data.items[0].imageUrl;
+      axios.get(res.data.data[26].data).then((res)=>{
+      	  console.log(res.data.data[5])
+          this.listContent5 = (res.data.data).slice(0,5);
+          
+          axios.get(res.data.data[5].data).then((res)=>{
+	      	  console.log(res)
+	          this.listContent5List = (res.data.data).slice(0,5);
+
+	      })
+      })
+  		
   		
 
   	})
@@ -149,7 +309,8 @@ export default {
   	   	  autoplay:true,
 	      pagination: {
 	        el: '.swiper-pagination',
-	      },
+	      }
+	  
      });
   },
   methods:{
@@ -162,7 +323,6 @@ export default {
 
   }
 }
-
 </script>
 <style >
 
